0c788df20cdfbbbeca6264e48b7a671997e928d1
Levin Keller <post@levinkeller.de>
2018-04-23 13:58:52 +0200

    Allow arbitrary recursion in TOC.

--- src/components/Layout/TableOfContents.jsx
+++ src/components/Layout/TableOfContents.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 
-const Links = ({ entries, index }) => (
+const Links = ({ entries }) => (
   <StyledLinkList>
     {entries.map(({ entry }, key) => (
-      <EntryListItem key={`link${index}${key}`}>
+      <EntryListItem key={`link${key}`}>
         <Link to={entry.childMarkdownRemark.fields.slug}>
           <EntryTitle>{entry.childMarkdownRemark.frontmatter.title}</EntryTitle>
         </Link>
@@ -14,38 +14,36 @@ const Links = ({ entries, index }) => (
   </StyledLinkList>
 )
 
-const Chapters = ({ chapters, index }) => (
-  <StyledSubchapterList>
-    {chapters.map(({ entries, title }, key) => (
-      <li key={`subchapter${index}${key}`}>
-        <SubchapterTitle>{title}</SubchapterTitle>
-        <Links entries={entries} index={`subchapter${index}${key}`} />
-      </li>
-    ))}
-  </StyledSubchapterList>
-)
-
-const chapterMapper = ({ chapters, entries, title }, index) => (
-  <li key={`chapter${index}`}>
-    <ChapterTitle>{title}</ChapterTitle>
-    {chapters && <Chapters chapters={chapters} index={index} />}
-    {entries && <Links entries={entries} index={index} />}
-  </li>
+const ChapterList = ({ chapters, entries, title, level = 0 }) => (
+  <StyledChapterList>
+    {title && <ChapterListItem key={`chapter${level}`}>
+      <ChapterTitle level={level}>{title}</ChapterTitle>
+    </ChapterListItem>}
+    <ChapterListItem>
+      {entries && <Links entries={entries} />}
+    </ChapterListItem>
+    <ChapterListItem>
+      {chapters && chapters.map(chapter => (
+        <ChapterList {...chapter} level={level + 1} />
+      ))}
+    </ChapterListItem>
+  </StyledChapterList>
 )
 
 const TableOfContents = ({ chapters }) => (
-  <StyledChapterList>{chapters.map(chapterMapper, [])}</StyledChapterList>
+  <TOCWrapper>{chapters.map(chapter => <ChapterList {...chapter} />)}</TOCWrapper>
 )
 
 export default TableOfContents
 
-const StyledChapterList = styled.ol`
-  list-style: none;
+const TOCWrapper = styled.div`
   padding: ${props => props.theme.sitePadding};
   margin: 0;
 `
-const StyledSubchapterList = styled.ol`
+
+const StyledChapterList = styled.ol`
   list-style: none;
+  margin: 0;
 `
 
 const StyledLinkList = styled.ol`
@@ -62,6 +60,10 @@ const EntryTitle = styled.h6`
   text-decoration: none;
 `
 
+const ChapterListItem = styled.li`
+  margin: 0;
+`
+
 const EntryListItem = styled.li`
   margin: 0;
   a:hover {
@@ -70,14 +72,44 @@ const EntryListItem = styled.li`
 `
 
 const ChapterTitle = styled.h5`
-  font-weight: 200;
-  font-size: 2.8rem;
-  color: ${props => props.theme.brand};
-  margin-bottom: 10px;
-`
-
-const SubchapterTitle = styled.h5`
-  font-weight: 600;
-  color: black;
-  margin-bottom: 5px;
+  font-weight: ${({ level }) => {
+    switch (level % 3) {
+      case 1:
+        return '600'
+      case 2:
+        return '400'
+      default:
+        return '200'
+    }
+  }};
+  font-size: ${({ level }) => {
+    switch (level % 3) {
+      case 1:
+        return '2.4rem'
+      case 2:
+        return '2rem'
+      default:
+        return '2.8rem'
+    }
+  }};
+  color: ${({ level, theme }) => {
+    switch (level % 3) {
+      case 1:
+        return 'black'
+      case 2:
+        return 'blue'
+      default:
+        return theme.brand
+    }
+  }};
+  margin-bottom: ${({ level }) => {
+    switch (level % 3) {
+      case 1:
+        return '7px'
+      case 2:
+        return '5px'
+      default:
+        return '10px'
+    }
+  }};
 `
