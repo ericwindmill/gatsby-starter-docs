9c4b3892bc39bbc54a521872168e654b812e48d4
Eric Windmill <ericwindmill@gmail.com>
2018-04-25 06:39:22 -0700

    add prettier command to webstorm, couple style nudges

--- src/components/Layout/TableOfContents.jsx
+++ src/components/Layout/TableOfContents.jsx
@@ -4,9 +4,9 @@ import styled from 'styled-components'
 
 /* eslint react/no-array-index-key: "off" */
 
-const Links = ({entries}) => (
+const Links = ({ entries }) => (
   <StyledLinkList>
-    {entries.map(({entry}, key) => (
+    {entries.map(({ entry }, key) => (
       <EntryListItem key={key}>
         <Link to={entry.childMarkdownRemark.fields.slug}>
           <EntryTitle>{entry.childMarkdownRemark.frontmatter.title}</EntryTitle>
@@ -16,25 +16,27 @@ const Links = ({entries}) => (
   </StyledLinkList>
 )
 
-const ChapterList = ({chapters, entries, title, level = 0}) => (
+const ChapterList = ({ chapters, entries, title, level = 0 }) => (
   <StyledChapterList>
-    {title && <ChapterListItem key={`${title}${level}`}>
-      <ChapterTitle level={level}>{title}</ChapterTitle>
-    </ChapterListItem>}
+    {title && (
+      <ChapterListItem key={`${title}${level}`}>
+        <ChapterTitle level={level}>{title}</ChapterTitle>
+      </ChapterListItem>
+    )}
+    <ChapterListItem>{entries && <Links entries={entries} />}</ChapterListItem>
     <ChapterListItem>
-      {entries && <Links entries={entries}/>}
-    </ChapterListItem>
-    <ChapterListItem>
-      {chapters && chapters.map((chapter, index) => (
-        <ChapterList {...chapter} level={level + 1} key={`${index}`}/>
-      ))}
+      {chapters &&
+        chapters.map((chapter, index) => (
+          <ChapterList {...chapter} level={level + 1} key={`${index}`} />
+        ))}
     </ChapterListItem>
   </StyledChapterList>
 )
 
-const TableOfContents = ({chapters}) => (
-  <TOCWrapper>{chapters.map((chapter, index) => <ChapterList {...chapter}
-                                                             key={index}/>)}</TOCWrapper>
+const TableOfContents = ({ chapters }) => (
+  <TOCWrapper>
+    {chapters.map((chapter, index) => <ChapterList {...chapter} key={index} />)}
+  </TOCWrapper>
 )
 
 export default TableOfContents
@@ -61,50 +63,48 @@ const EntryTitle = styled.h6`
   line-height: 1.5;
   border-bottom: 1px solid transparent;
   text-decoration: none;
-  `
+`
 
 const ChapterListItem = styled.li`
   margin: 0;
-  `
+`
 
 const EntryListItem = styled.li`
   margin: 0;
-  a:hover
-  {
+  a:hover {
     border-bottom: 1px solid black;
   }
-  `
+`
 
 const ChapterTitle = styled.h5`
-  font - weight
-: ${({level}) => {
-  switch (level % 3) {
-    case 1:
-      return '600'
-    case 2:
-      return '400'
-    default:
-      return '200'
-  }
-}};
-  font-size: ${({level}) => {
-  switch (level % 3) {
-    case 1:
-      return '2.2rem'
-    case 2:
-      return '2rem'
-    default:
-      return '2.8rem'
-  }
-}};
-  color: ${({level, theme}) => {
-  switch (level % 3) {
-    case 1:
-      return 'black'
-    case 2:
-      return 'black'
-    default:
-      return theme.brand
-  }
-}};
+  font-weight: ${({ level }) => {
+    switch (level % 3) {
+      case 1:
+        return '600'
+      case 2:
+        return '400'
+      default:
+        return '200'
+    }
+  }};
+  font-size: ${({ level }) => {
+    switch (level % 3) {
+      case 1:
+        return '2.2rem'
+      case 2:
+        return '1.8rem'
+      default:
+        return '2.8rem'
+    }
+  }};
+  color: ${({ level, theme }) => {
+    switch (level % 3) {
+      case 1:
+        return 'black'
+      case 2:
+        return theme.accent
+      default:
+        return theme.brand
+    }
+  }};
 `
